4d64502029c9edd5566a88a9f84f46e0bc7ed385
mks <mks@gmail.com>
2018-07-10 19:10:36 +0430

    add ApiResponse object for Retrofit response create new dialog init searching on application start remove start button

--- app/src/main/java/com/example/mohammadrezasadeghi/parkml/FunctionCallsReceiver.java
+++ app/src/main/java/com/example/mohammadrezasadeghi/parkml/FunctionCallsReceiver.java
@@ -8,19 +8,6 @@ import android.support.v4.app.NotificationCompat;
 import android.support.v4.app.NotificationManagerCompat;
 import android.util.Log;
 
-import com.android.volley.Request;
-import com.android.volley.Response;
-import com.android.volley.VolleyError;
-import com.android.volley.toolbox.JsonObjectRequest;
-
-import org.json.JSONException;
-import org.json.JSONObject;
-
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.net.URL;
-import java.net.URLConnection;
 import java.sql.Timestamp;
 import java.util.HashMap;
 import java.util.Map;
@@ -30,31 +17,39 @@ import hrituc.studenti.uniroma1.it.generocityframework.ParkingLocation;
 import hrituc.studenti.uniroma1.it.generocityframework.TripPoint;
 import retrofit2.Call;
 import retrofit2.Callback;
+import retrofit2.Response;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class FunctionCallsReceiver extends BroadcastReceiver {
+
+    private HomeActivity homeActivity;
+
+    public FunctionCallsReceiver(HomeActivity homeActivity) {
+        this.homeActivity = homeActivity;
+    }
+
     @Override
     public void onReceive(Context context, Intent intent) {
-        if (Constants.LOGS) Log.e( "FunctionCallsReceiver", "onReceive called" );
+        if (Constants.LOGS) Log.e("FunctionCallsReceiver", "onReceive called");
         String action = intent.getAction();
-        if (Constants.DID_ENTER_CAR.equals( action )) {
+        if (Constants.DID_ENTER_CAR.equals(action)) {
 
             //get the bluetooth device that the user is connected to. BluetoothDevice.EXTRA_DEVICE is
             //an android constant, so since it was there I decided to use it.
-            BluetoothDevice connectedDevice = intent.getParcelableExtra( BluetoothDevice.EXTRA_DEVICE );
+            BluetoothDevice connectedDevice = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
 
-            NotificationCompat.Builder builder = new NotificationCompat.Builder( context, "isInCar" );
-            builder.setContentText( "Did enter car" );
-            builder.setSmallIcon( hrituc.studenti.uniroma1.it.generocityframework.R.mipmap.ic_launcher );
-            builder.setContentTitle( "FunctionCallsReceiver" );
-            NotificationManagerCompat.from( context ).notify( 7, builder.build() );
-        } else if (Constants.DID_EXIT_CAR.equals( action )) {
+            NotificationCompat.Builder builder = new NotificationCompat.Builder(context, "isInCar");
+            builder.setContentText("Did enter car");
+            builder.setSmallIcon(hrituc.studenti.uniroma1.it.generocityframework.R.mipmap.ic_launcher);
+            builder.setContentTitle("FunctionCallsReceiver");
+            NotificationManagerCompat.from(context).notify(7, builder.build());
+        } else if (Constants.DID_EXIT_CAR.equals(action)) {
             //extract the parking position
-            ParkingLocation parkingLocation = ParkingLocation.fromIntent( context, intent );
+            ParkingLocation parkingLocation = ParkingLocation.fromIntent(context, intent);
 
             //get the device that the user was connected to
-            BluetoothDevice previouslyConnectedDevice = intent.getParcelableExtra( BluetoothDevice.EXTRA_DEVICE );
+            BluetoothDevice previouslyConnectedDevice = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
 
             //The parking location may be null in some rare cases, like if the user manually disables
             //the position setting on his phone
@@ -63,81 +58,76 @@ public class FunctionCallsReceiver extends BroadcastReceiver {
             if (parkingLocation != null) {
                 latitude = parkingLocation.getLatitude();
                 longitude = parkingLocation.getLongitude();
-                if (Constants.LOGS) Log.e( "FunctionCallsReceiver", "parking location extracted" );
+                if (Constants.LOGS) Log.e("FunctionCallsReceiver", "parking location extracted");
             }
-            NotificationCompat.Builder builder = new NotificationCompat.Builder( context, "isInCar" );
-            builder.setContentText( "Did exit car on lat: " + latitude + " - lon: " + longitude );
-            builder.setSmallIcon( hrituc.studenti.uniroma1.it.generocityframework.R.mipmap.ic_launcher );
-            builder.setContentTitle( "FunctionCallsReceiver" );
-            NotificationManagerCompat.from( context ).notify( 8, builder.build() );
-        } else if (Constants.WILL_EXIT_CAR.equals( action )) {
-            NotificationCompat.Builder builder = new NotificationCompat.Builder( context, "willExit" );
-            builder.setContentText( "Will exit car" );
-            builder.setSmallIcon( hrituc.studenti.uniroma1.it.generocityframework.R.mipmap.ic_launcher );
-            builder.setContentTitle( "FunctionCallsReceiver" );
-            NotificationManagerCompat.from( context ).notify( 9, builder.build() );
-        } else if (Constants.TRIP_POINT.equals( action )) {
+            NotificationCompat.Builder builder = new NotificationCompat.Builder(context, "isInCar");
+            builder.setContentText("Did exit car on lat: " + latitude + " - lon: " + longitude);
+            builder.setSmallIcon(hrituc.studenti.uniroma1.it.generocityframework.R.mipmap.ic_launcher);
+            builder.setContentTitle("FunctionCallsReceiver");
+            NotificationManagerCompat.from(context).notify(8, builder.build());
+        } else if (Constants.WILL_EXIT_CAR.equals(action)) {
+            NotificationCompat.Builder builder = new NotificationCompat.Builder(context, "willExit");
+            builder.setContentText("Will exit car");
+            builder.setSmallIcon(hrituc.studenti.uniroma1.it.generocityframework.R.mipmap.ic_launcher);
+            builder.setContentTitle("FunctionCallsReceiver");
+            NotificationManagerCompat.from(context).notify(9, builder.build());
+        } else if (Constants.TRIP_POINT.equals(action)) {
             //extract the data from the intent
-            TripPoint tripPoint = TripPoint.fromIntent( intent );
+            TripPoint tripPoint = TripPoint.fromIntent(intent);
             double lat = tripPoint.getLatitude();
             double lon = tripPoint.getLongitude();
             float speed = tripPoint.getSpeed();
             Timestamp timestamp = tripPoint.getTimestamp();
             float Parked = tripPoint.getSpeed();
 
-          
-
             Retrofit retrofit = new Retrofit.Builder()
-                    .baseUrl("https://server-parkml.apps.os.sapienzaapps.it/").
+                    .baseUrl("http://192.168.1.102:8080").
                     addConverterFactory( GsonConverterFactory.create())
                     .build();
 
-            ParkMLAPIInterface intf = retrofit.create( ParkMLAPIInterface.class );
+            ParkMLAPIInterface intf = retrofit.create(ParkMLAPIInterface.class);
 
             Map<String, Object> postData = new HashMap<>();
-            postData.put( "Latitude", lat );
-            postData.put( "Longitude", lon );
-            postData.put( "Speed", speed );
-            postData.put( "Timestamp", timestamp );
-            postData.put( "Parked", Parked);
+            postData.put("Latitude", lat);
+            postData.put("Longitude", lon);
+            postData.put("Speed", speed);
+            postData.put("Timestamp", timestamp);
+            postData.put("Parked", Parked);
 
-            Call<String> res = intf.api( postData );
-            res.enqueue( new Callback<String>() {
+            Call<ApiResponse> res = intf.api(postData);
+            res.enqueue(new Callback<ApiResponse>() {
                 @Override
-                public void onResponse(Call<String> call, retrofit2.Response<String> response) {
-                    Log.d("FunctionCallsReceiver", "Response: " + response.body());
-                    if ("1".equals(response.body())) {
-
-
+                public void onResponse(Call<ApiResponse> call, Response<ApiResponse> response) {
+                    Log.d("FunctionCallsReceiver", "Response: " + response.body().getResults());
+                    if ("YES".equals(response.body().getResults())) {
+                        homeActivity.openDialog3();
                     } else {
 
                     }
                 }
 
                 @Override
-                public void onFailure(Call<String> call, Throwable t) {
+                public void onFailure(Call<ApiResponse> call, Throwable t) {
                     Log.e("FunctionCallsReceiver", "Error: " + t.getMessage());
                 }
-            } );
+            });
 
-            
 
-            if (Constants.LOGS) Log.e( "FunctionCallsReceiver", "trip-point extracted" );
-        } else if (Constants.WILL_ENTER_CAR.equals( action )) {
-            NotificationCompat.Builder builder = new NotificationCompat.Builder( context, "willEnter" );
-            builder.setContentText( "Will enter car" );
-            builder.setSmallIcon( hrituc.studenti.uniroma1.it.generocityframework.R.mipmap.ic_launcher );
-            builder.setContentTitle( "FunctionCallsReceiver" );
-            NotificationManagerCompat.from( context ).notify( 10, builder.build() );
-        } else if (action.equals( "UPDATE_GEOFENCE_LIST" )) {
-            String id = intent.getStringExtra( "id" );
-            double latitude = intent.getDoubleExtra( "latitude", -1 );
-            double longitude = intent.getDoubleExtra( "longitude", -1 );
+            if (Constants.LOGS) Log.e("FunctionCallsReceiver", "trip-point extracted");
+        } else if (Constants.WILL_ENTER_CAR.equals(action)) {
+            NotificationCompat.Builder builder = new NotificationCompat.Builder(context, "willEnter");
+            builder.setContentText("Will enter car");
+            builder.setSmallIcon(hrituc.studenti.uniroma1.it.generocityframework.R.mipmap.ic_launcher);
+            builder.setContentTitle("FunctionCallsReceiver");
+            NotificationManagerCompat.from(context).notify(10, builder.build());
+        } else if (action.equals("UPDATE_GEOFENCE_LIST")) {
+            String id = intent.getStringExtra("id");
+            double latitude = intent.getDoubleExtra("latitude", -1);
+            double longitude = intent.getDoubleExtra("longitude", -1);
             // MainActivity.updateGefenceMap(id,latitude,longitude);
         }
     }
 }
-
 
 
 //do stuff with these
--- app/src/main/java/com/example/mohammadrezasadeghi/parkml/ParkMLAPIInterface.java
+++ app/src/main/java/com/example/mohammadrezasadeghi/parkml/ParkMLAPIInterface.java
@@ -4,11 +4,13 @@ import java.util.Map;
 
 import retrofit2.Call;
 import retrofit2.http.Body;
+import retrofit2.http.Headers;
 import retrofit2.http.POST;
 
 public interface ParkMLAPIInterface {
 
+    @Headers("Content-Type: application/json")
     @POST("api")
-    Call<String> api(@Body Map<String, Object> body);
+    Call<ApiResponse> api(@Body Map<String, Object> body);
 
 }
